ce0fdcc210a6111b92ff82ba5bb96240b81ab60d
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-02-18 19:53:03 +0000

    -- ISSIntel.GravarXml -- [*] ACBR-8964 - Adiciona CodigoServicoNacional no grupo serviço conforme novos schemas.    Por: Valter Patrick
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44920 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/ISSIntel.GravarXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/ISSIntel.GravarXml.pas
@@ -37,7 +37,7 @@ unit ISSIntel.GravarXml;
 interface
 
 uses
-  SysUtils, Classes, StrUtils,
+  SysUtils, Classes, StrUtils, ACBrXmlDocument, ACBrXmlBase, ACBrDFe.Conversao, ACBrNFSeXConsts, ACBrUtil.Strings,
   ACBrNFSeXGravarXml_ABRASFv1;
 
 type
@@ -46,6 +46,7 @@ type
   TNFSeW_ISSIntel = class(TNFSeW_ABRASFv1)
   protected
     procedure Configuracao; override;
+	function GerarServico: TACBrXmlNode; override;
 
   public
     function GerarXml: Boolean; Override;
@@ -73,6 +74,9 @@ begin
     DivAliq100 := False;
 
   FormatoItemListaServico := filsComFormatacaoSemZeroEsquerda;
+  
+  if FpAOwner.ConfigGeral.Params.TemParametro('NaoFormatarItemServico') then
+    FormatoItemListaServico := filsSemFormatacao;
 end;
 
 function TNFSeW_ISSIntel.GerarXml: Boolean;
@@ -82,6 +86,50 @@ begin
     NrOcorrAliquota := 1;
 
   Result := inherited GerarXml;
+end;
+
+function TNFSeW_ISSIntel.GerarServico: TACBrXmlNode;
+var
+  nodeArray: TACBrXmlNodeArray;
+  i: Integer;
+  item: string;
+begin
+  // Em conformidade com a versão 1 do layout da ABRASF não deve ser alterado
+  Result := CreateElement('Servico');
+
+  Result.AppendChild(GerarValores);
+
+  item := FormatarItemServico(NFSe.Servico.ItemListaServico, FormatoItemListaServico);
+
+  Result.AppendChild(AddNode(tcStr, '#29', 'ItemListaServico', 1, 5, NrOcorrItemListaServico,
+                                                          item, DSC_CLISTSERV));
+
+  Result.AppendChild(AddNode(tcStr, '#29', 'CodigoServicoNacional', 1, 10, 1, NFSe.Servico.CodigoServicoNacional, ''));
+
+  Result.AppendChild(AddNode(tcStr, '#30', 'CodigoCnae', 1, 7, NrOcorrCodigoCnae,
+                                OnlyNumber(NFSe.Servico.CodigoCnae), DSC_CNAE));
+
+  Result.AppendChild(AddNode(tcStr, '#31', 'CodigoTributacaoMunicipio', 1, 20, NrOcorrCodTribMun,
+                     NFSe.Servico.CodigoTributacaoMunicipio, DSC_CSERVTRIBMUN));
+
+  Result.AppendChild(AddNode(tcStr, '#32', 'Discriminacao', 1, 2000, 1,
+    StringReplace(NFSe.Servico.Discriminacao, Opcoes.QuebraLinha,
+               FpAOwner.ConfigGeral.QuebradeLinha, [rfReplaceAll]), DSC_DISCR));
+
+  Result.AppendChild(AddNode(tcStr, '#', 'InformacoesComplementares', 1, 255, NrOcorrInformacoesComplemetares,
+    StringReplace(NFSe.InformacoesComplementares, Opcoes.QuebraLinha,
+                      FpAOwner.ConfigGeral.QuebradeLinha, [rfReplaceAll]), ''));
+
+  Result.AppendChild(GerarServicoCodigoMunicipio);
+
+  nodeArray := GerarItensServico;
+  if nodeArray <> nil then
+  begin
+    for i := 0 to Length(nodeArray) - 1 do
+    begin
+      Result.AppendChild(nodeArray[i]);
+    end;
+  end;
 end;
 
 end.
