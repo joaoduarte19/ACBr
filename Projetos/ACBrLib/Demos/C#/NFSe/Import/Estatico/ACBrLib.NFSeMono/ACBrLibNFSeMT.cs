8c3814c3f30b89f30839e403c07c41ced9ccbef9
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-02-18 23:08:22 +0000

    18/02/2026 -- ACBrLibNFSeMT.cs -- [-] Remove código não necessário
     Por: doszapro
    
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44932 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/NFSe/Import/Estatico/ACBrLib.NFSeMono/ACBrLibNFSeMT.cs
+++ Projetos/ACBrLib/Demos/C#/NFSe/Import/Estatico/ACBrLib.NFSeMono/ACBrLibNFSeMT.cs
@@ -144,12 +144,7 @@ public class ACBrLibNFSeMT : ACBrLibBase, IACBrLibNFSe, IDisposable
 
     public override T ConfigLerValor<T>(ACBrSessao eSessao, string eChave)
     {
-        var bufferLen = BUFFER_LEN;
-        var pValue = new StringBuilder(bufferLen);
-        var ret = ACBrLibNFSeBridgeMT.NFSE_ConfigLerValor(libHandle, ToUTF8(eSessao.ToString()), ToUTF8(eChave), pValue, ref bufferLen);
-        CheckResult(ret);
-
-        var value = CheckBuffer(pValue, bufferLen);
+        var value = ConfigLerValor(eSessao.ToString(), eChave);
         return ConvertValue<T>(value);
     }
 
@@ -158,9 +153,7 @@ public class ACBrLibNFSeMT : ACBrLibBase, IACBrLibNFSe, IDisposable
         if (value == null) return;
 
         var propValue = ConvertValue(value);
-
-        var ret = ACBrLibNFSeBridgeMT.NFSE_ConfigGravarValor(libHandle, ToUTF8(eSessao.ToString()), ToUTF8(eChave), ToUTF8(propValue));
-        CheckResult(ret);
+        ConfigGravarValor(eSessao.ToString(), eChave, propValue);
     }
 
 
