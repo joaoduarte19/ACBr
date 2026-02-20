c5c71c4cf6453ec0e4e6ac7d8f1c7bae497731ba
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-02-20 12:35:38 +0000

    20/02/2026 -- ModernizacaoPublica.Provider -- [-] ACBR-8948 - Correção para emissão em produção, requer informar Usuário e senha em Basic Auth.  Por: Jefferson Junglaus
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44955 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/ModernizacaoPublica.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/ModernizacaoPublica.Provider.pas
@@ -48,7 +48,9 @@ uses
   PadraoNacional.Provider,
   ACBrNFSeXWebserviceBase,
   ACBrNFSeXWebservicesResponse,
-  ACBrJson;
+  ACBrJson,
+  ACBrBase,
+  synacode;
 
 type
   TACBrNFSeXWebserviceModernizacaoPublica202 = class(TACBrNFSeXWebserviceSoap11)
@@ -83,7 +85,7 @@ type
 
   TACBrNFSeXWebserviceModernizacaoPublicaAPIPropria = class(TACBrNFSeXWebservicePadraoNacional)
   protected
-
+    procedure SetHeaders(aHeaderReq: THTTPHeader); override;
   public
 
     function TratarXmlRetornado(const aXML: string): string; override;
@@ -554,6 +556,19 @@ begin
 end;
 
 { TACBrNFSeXWebserviceModernizacaoPublicaAPIPropria }
+
+procedure TACBrNFSeXWebserviceModernizacaoPublicaAPIPropria.SetHeaders(
+  aHeaderReq: THTTPHeader);
+var
+  Auth: string;
+begin
+  // Necessário para emitir em Produção
+  with TConfiguracoesNFSe(FPConfiguracoes).Geral.Emitente do
+    Auth := 'Basic ' + string(EncodeBase64(AnsiString(WSUser + ':' +
+      AnsiString(WSSenha))));
+
+  aHeaderReq.AddHeader('Authorization', Auth);
+end;
 
 function TACBrNFSeXWebserviceModernizacaoPublicaAPIPropria.TratarXmlRetornado(
   const aXML: string): string;
