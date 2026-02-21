6c98ce8ae73a430f0a862fc3a285cb736ce4fd4d
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-02-21 18:32:56 +0000

    21/02/2026 -- ACBrLib NFSe C# -- [+] Adiciona documentação nas classes de configuração [+] Atualiza pacote no nuget
      Por: doszapro
    
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@44977 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/NFSe/Import/Dinamico/Shared/ACBrNFSeConfig.cs
+++ Projetos/ACBrLib/Demos/C#/NFSe/Import/Dinamico/Shared/ACBrNFSeConfig.cs
@@ -5,10 +5,18 @@ using ACBrLib.Core.NFSe;
 
 namespace ACBrLib.NFSe
 {
+    /// <summary>
+    /// Configurações específicas da ACBrLibNFSe, conforme documentação oficial da ACBrLib.
+    /// Veja mais em: https://acbr.sourceforge.io/ACBrLib/ConfiguracoesdaBiblioteca22.html
+    /// </summary>
     public sealed class ACBrNFSeConfig : ACBrLibDFeConfig<IACBrLibNFSe>
     {
         #region Constructors
 
+        /// <summary>
+        /// Inicializa uma nova instância da classe ACBrNFSeConfig para configuração da ACBrLibNFSe.
+        /// </summary>
+        /// <param name="acbrlib">Instância da interface de <see cref="IACBrLibNFSe"/>.</param>
         public ACBrNFSeConfig(IACBrLibNFSe acbrlib) : base(acbrlib, ACBrSessao.NFSe)
         {
             DANFSe = new DANFSeConfig(acbrlib);
@@ -18,50 +26,86 @@ namespace ACBrLib.NFSe
 
         #region Properties
 
+        /// <summary>
+        /// Configurações do DANFSe (Documento Auxiliar da Nota Fiscal de Serviço Eletrônica).
+        /// </summary>
         public DANFSeConfig DANFSe { get; }
 
+        /// <summary>
+        /// Caminho onde será salvo o XML da NFSe.
+        /// Chave: PathNFSe
+        /// </summary>
         public string PathNFSe
         {
             get => GetProperty<string>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se o XML de cancelamento deve ser atualizado.
+        /// Chave: AtualizarXMLCancelado
+        /// </summary>
         public bool AtualizarXMLCancelado
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se o PathSchema deve ser montado automaticamente.
+        /// Chave: MontarPathSchema
+        /// </summary>
         public bool MontarPathSchema
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se deve consultar o lote após o envio.
+        /// Chave: ConsultaLoteAposEnvio
+        /// </summary>
         public bool ConsultaLoteAposEnvio
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se deve consultar a NFSe após o cancelamento.
+        /// Chave: ConsultaAposCancelar
+        /// </summary>
         public bool ConsultaAposCancelar
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define o layout da NFSe utilizado.
+        /// Chave: LayoutNFSe
+        /// 0 = lnfsProvedor, 1 = lnfsPadraoNacionalv1, 2 = lnfsPadraoNacionalv101
+        /// </summary>
         public LayoutNFSe LayoutNFSe
         {
             get => GetProperty<LayoutNFSe>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se será utilizada a emissão de Path específico para NFSe.
+        /// Chave: EmissaoPathNFSe
+        /// </summary>
         public bool EmissaoPathNFSe
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define o código do município utilizado nas operações da NFSe.
+        /// Chave: CodigoMunicipio
+        /// </summary>
         public CodigoMunicipio CodigoMunicipio
         {
             get => GetProperty<CodigoMunicipio>();
--- Projetos/ACBrLib/Demos/C#/NFSe/Import/Dinamico/Shared/DANFSeConfig.cs
+++ Projetos/ACBrLib/Demos/C#/NFSe/Import/Dinamico/Shared/DANFSeConfig.cs
@@ -5,10 +5,18 @@ using ACBrLib.Core.NFSe;
 
 namespace ACBrLib.NFSe
 {
+    /// <summary>
+    /// Configurações do DANFSe (Documento Auxiliar da Nota Fiscal de Serviço Eletrônica) para ACBrLibNFSe.
+    /// Veja mais em: https://acbr.sourceforge.io/ACBrLib/ConfiguracoesdaBiblioteca22.html
+    /// </summary>
     public sealed class DANFSeConfig : ReportConfig<IACBrLibNFSe>
     {
         #region Constructors
 
+        /// <summary>
+        /// Inicializa uma nova instância da classe DANFSeConfig para configuração do DANFSe na ACBrLibNFSe.
+        /// </summary>
+        /// <param name="acbrlib">Instância da interface de <see cref="IACBrLibNFSe"/>.</param>
         public DANFSeConfig(IACBrLibNFSe acbrlib) : base(acbrlib, ACBrSessao.DANFSe)
         {
 
@@ -18,36 +26,63 @@ namespace ACBrLib.NFSe
 
         #region Properties
         
+        /// <summary>
+        /// Define o tipo de DANFSe a ser gerado.
+        /// Chave: TipoDANFSE
+        /// </summary>
         public TipoDANFSE TipoDANFSE
         {
             get => GetProperty<TipoDANFSE>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define o nome da Prefeitura a ser exibido no DANFSe.
+        /// Chave: Prefeitura
+        /// </summary>
         public string Prefeitura
         {
             get => GetProperty<string>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se o DANFSe será impresso com a tarja de cancelada.
+        /// Chave: Cancelada
+        /// 0 = Não, 1 = Sim
+        /// </summary>
         public bool Cancelada
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se o ambiente é de produção (não exibe "Ambiente de Homologação - SEM VALOR FISCAL").
+        /// Chave: Producao
+        /// 0 = Não (exibe mensagem de homologação), 1 = Sim (não exibe)
+        /// </summary>
         public bool Producao
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se o DANFSe detalhará os serviços.
+        /// Chave: DetalharServico
+        /// 0 = Não, 1 = Sim
+        /// </summary>
         public bool DetalharServico
         {
             get => GetProperty<bool>();
             set => SetProperty(value);
         }
 
+        /// <summary>
+        /// Define se a data de competência será exibida completa no DANFSe.
+        /// Chave: DataCompetenciaCompleta
+        /// </summary>
         public bool DataCompetenciaCompleta
         {
             get => GetProperty<bool>();
