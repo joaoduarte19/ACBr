6264909905ce59f6348da7217db9b7092d236819
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-02-25 12:34:42 +0000

    25/02/2026 -- ACBrCTeConhecimentos -- [*] Alteração visando remover se necessário os bytes do UTF8 BOM ao carregar um XML.    Por: Italo Giurizzato Junior
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@45056 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrCTe/ACBrCTeConhecimentos.pas
+++ Fontes/ACBrDFe/ACBrCTe/ACBrCTeConhecimentos.pas
@@ -1068,6 +1068,7 @@ begin
   try
     MS.LoadFromFile(CaminhoArquivo);
     XMLUTF8 := ReadStrFromStream(MS, MS.Size);
+    XMLUTF8 := RemoverUTF8Bom(XMLUTF8);
   finally
     MS.Free;
   end;
@@ -1114,7 +1115,9 @@ var
 
 begin
   // Verifica se precisa Converter de UTF8 para a String nativa da IDE //
-  XMLStr := ConverteXMLtoNativeString(AXMLString);
+  XMLStr := RemoverUTF8Bom(AXMLString);
+  XMLStr := ConverteXMLtoNativeString(XMLStr);
+  XMLStr := RemoverDeclaracaoXML(XMLStr);
 
   if Pos('</CTeOS>', XMLStr) > 0 then
     Modelo := moCTeOS
