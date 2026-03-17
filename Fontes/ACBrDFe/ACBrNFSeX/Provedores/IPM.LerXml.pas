651856637db753be249949336ab09e232d7df89b
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-03-17 17:45:19 +0000

    17/03/2026 -- IPM.LerXml -- [*] ACBR-8997 - Incluído a leitura da tag chave_acesso_nfse_nacional.    Por: Diogo Loff
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@45342 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.LerXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.LerXml.pas
@@ -255,6 +255,8 @@ begin
       if Trim(CodigoVerificacao) = '' then
         CodigoVerificacao := ObterConteudo(AuxNode.Childrens.FindAnyNs('cod_verificador_autenticidade'), tcStr);
 
+      ChaveAcesso := ObterConteudo(AuxNode.Childrens.FindAnyNs('chave_acesso_nfse_nacional'), tcStr);
+
       Link := ObterConteudo(AuxNode.Childrens.FindAnyNs('link_nfse'), tcStr);
       Link := StringReplace(Link, '&amp;', '&', [rfReplaceAll]);
 
