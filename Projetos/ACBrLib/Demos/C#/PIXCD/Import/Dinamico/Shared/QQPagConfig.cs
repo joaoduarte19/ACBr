f3a8db399165d872948cb7764551b3f847722395
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-04-20 17:49:54 +0000

    Classe configuração QQPag
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@45844 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/PIXCD/Import/Dinamico/Shared/QQPagConfig.cs
@@ -0,0 +1,65 @@
+﻿using ACBrLib.Core;
+using ACBrLib.Core.Config;
+using ACBrLib.Core.DFe;
+using ACBrLib.Core.PIXCD;
+
+namespace ACBrLib.PIXCD
+{
+    /// <summary>
+    /// Configurações específicas do PSP Quero Quero Pag para PIXCD.
+    /// Veja todas as chaves e descrições em: https://acbr.sourceforge.io/ACBrLib/ConfiguracoesdaBiblioteca23.html
+    /// </summary>
+    public sealed class QQPagConfig : ACBrLibConfigBase<IACBrLibPIXCD>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Inicializa as configurações do PSP Quero Quero Pag.
+        /// </summary>
+        /// <param name="acbrlib">Instância da interface PIXCD.</param>
+        public QQPagConfig(IACBrLibPIXCD acbrlib) : base(acbrlib, ACBrSessao.QQPag)
+        {
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Chave PIX utilizada para transações.
+        /// </summary>
+        public string ChavePIX
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        /// <summary>
+        /// ClientID para autenticação no Quero Quero Pag.
+        /// </summary>
+        public string ClientID
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        /// <summary>
+        /// ClientSecret para autenticação no Quero Quero Pag.
+        /// </summary>
+        public string ClientSecret
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+		
+        /// <summary>
+        /// Scopes enviados para a API. Exemplo: [scCobWrite,scCobRead,scPixWrite,scPixRead]
+        /// </summary>
+        public string Scopes
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+        #endregion Properties
+    }
+}
