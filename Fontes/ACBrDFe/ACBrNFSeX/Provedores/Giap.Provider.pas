54b5e8066c1d6794f61bc8717b5f2ef0c483b8bc
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-04-30 17:16:29 +0000

    30/04/2026 -- Giap.Provider -- [*] ACBr-9247 - Ajustes no ProcessarMensagemErros visando a leitura correta das     mensagens de erro.    Por: Store Brasil
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@46022 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Giap.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Giap.Provider.pas
@@ -217,31 +217,11 @@ end;
 procedure TACBrNFSeProviderGiap.ProcessarMensagemErros(
   RootNode: TACBrXmlNode; Response: TNFSeWebserviceResponse;
   const AListTag, AMessageTag: string);
-
-  function ObterValor(ANode: TACBrXmlNode; const ATag: string): string;
-  var
-    Node: TACBrXmlNode;
-    Attr: TACBrXmlAttribute;
-  begin
-    Attr := ANode.Attributes.Items[ATag];
-    if Attr <> nil then
-      Result := ObterConteudoTag(Attr)
-    else
-    begin
-      Node := ANode.Childrens.FindAnyNs(ATag);
-      if Node <> nil then
-        Result := ObterConteudoTag(Node, tcStr)
-      else
-        Result := '';
-    end;
-  end;
-
 var
-  I: Integer;
-  ANode: TACBrXmlNode;
-  ANodeArray: TACBrXmlNodeArray;
+  ANode, MsgNode: TACBrXmlNode;
   AErro: TNFSeEventoCollectionItem;
-  Codigo, Descricao: string;
+  Status: Integer;
+  Descricao: string;
 begin
   ANode := RootNode.Document.Root.Childrens.FindAnyNs('notaFiscal');
 
@@ -249,25 +229,25 @@ begin
     ANode := RootNode.Document.Root;
 
   if not Assigned(ANode) then
-    exit;
+    Exit;
 
-  if ObterConteudoTag(ANode.Childrens.FindAnyNs('statusEmissao'), tcInt) <> 200 then
+  Status := ObterConteudoTag(ANode.Childrens.FindAnyNs('statusEmissao'), tcInt);
+
+  if Status <> 200 then
   begin
-    ANodeArray := ANode.Childrens.FindAllAnyNs('messages');
+    MsgNode := ANode.Childrens.FindAnyNs('messages');
 
-    for I := Low(ANodeArray) to High(ANodeArray) do
+    if Assigned(MsgNode) then
     begin
-      ANode := ANodeArray[I];
+      Descricao := ObterConteudoTag(MsgNode, tcStr);
 
-      Codigo := ObterValor(ANode, 'code');
-      Descricao := ObterValor(ANode, 'message');
-
-      if (Codigo = '') and (Descricao = '') then Continue;
-
-      AErro := Response.Erros.New;
-      AErro.Codigo := ObterValor(ANode, 'code');
-      AErro.Descricao := ObterValor(ANode, 'message');
-      AErro.Correcao := '';
+      if Descricao <> '' then
+      begin
+        AErro := Response.Erros.New;
+        AErro.Codigo := IntToStr(Status);
+        AErro.Descricao := Descricao;
+        AErro.Correcao := '';
+      end;
     end;
   end;
 end;
