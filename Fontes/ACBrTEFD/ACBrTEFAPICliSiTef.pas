2693dec47d9a44b8e8b1aa77ea2dc41ef2f5fb40
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-06-19 20:39:12 +0000

    -- ACBrTEFAPICliSiTef -- [*] Ajuste para perguntar Campo 500-Supervior, com informações de Mascara e validação     https://www.projetoacbr.com.br/forum/index.php?showtopic=92968     (por: DSA)
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@46801 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTEFD/ACBrTEFAPICliSiTef.pas
+++ Fontes/ACBrTEFD/ACBrTEFAPICliSiTef.pas
@@ -690,6 +690,12 @@ begin
               if (ProximoComando = 41) then
                 DefinicaoCampo.OcultarDadosDigitados := True;
 
+              if (TipoCampo = 500) then
+              begin
+                DefinicaoCampo.ValidacaoDado := valdSenhaGerente;
+                DefinicaoCampo.OcultarDadosDigitados := True;
+              end;
+
               RespBuffer := DoPerguntarCampo(DefinicaoCampo);
             end;
 
