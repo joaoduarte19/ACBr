054acce79495549e8473259ae3fd920a87cd24ce
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-07-24 14:17:05 +0000

    24/07/2026 -- Saatri.GravarXml -- [*] ACBR-9629 - Alterações visando gerar os novos campos referente a RTC.    Por: Italo Giurizzato Junior
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@47366 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Saatri.GravarXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Saatri.GravarXml.pas
@@ -38,7 +38,9 @@ interface
 
 uses
   SysUtils, Classes, StrUtils,
-  ACBrNFSeXGravarXml_ABRASFv2;
+  ACBrNFSeXGravarXml_ABRASFv2,
+  ACBrXmlDocument,
+  ACBrNFSeXClass;
 
 type
   { TNFSeW_Saatri201 }
@@ -58,11 +60,21 @@ type
   protected
     procedure Configuracao; override;
 
+    function GerarInfDeclaracaoPrestacaoServico: TACBrXmlNode; override;
+    function GerarServico: TACBrXmlNode; override;
+    function GerarValores: TACBrXmlNode; override;
+    function GerarIBSCBS(IBSCBS: TIBSCBSDPS): TACBrXmlNode;
+    function GerarIBSMunicipal: TACBrXmlNode;
+    function GerarIBSEstadual: TACBrXmlNode;
+    function GerarCBS: TACBrXmlNode;
   end;
 
 implementation
 
 uses
+  ACBrDFe.Conversao,
+  ACBrNFSeXConversao,
+  ACBrNFSeXConsts,
   ACBrUtil.Strings;
 
 //==============================================================================
@@ -106,6 +118,172 @@ begin
 
   NrOcorrDiscriminacao_2 := 1;
   NrOcorrCodigoMunic_2 := 1;
+end;
+
+function TNFSeW_Saatri203.GerarInfDeclaracaoPrestacaoServico: TACBrXmlNode;
+var
+  aNameSpace: string;
+  nodeArray: TACBrXmlNodeArray;
+  i: Integer;
+begin
+  aNameSpace := DefinirNameSpaceDeclaracao;
+
+  Result := CreateElement('InfDeclaracaoPrestacaoServico');
+
+  if aNameSpace <> '' then
+    Result.SetNamespace(aNameSpace);
+
+  DefinirIDDeclaracao;
+
+  if (FpAOwner.ConfigGeral.Identificador <> '') and GerarIDDeclaracao then
+    Result.SetAttribute(FpAOwner.ConfigGeral.Identificador, NFSe.infID.ID);
+
+  Result.AppendChild(AddNode(tcStr, '#4', 'Id', 1, 15, NrOcorrID,
+                                                            NFSe.infID.ID, ''));
+
+  if (NFSe.IdentificacaoRps.Numero <> '') and GerarTagRps then
+    Result.AppendChild(GerarRps);
+
+  Result.AppendChild(GerarListaServicos);
+
+  Result.AppendChild(AddNode(FormatoCompetencia, '#4', 'Competencia', 10, 10, 1,
+                                                  NFSe.Competencia, DSC_DHEMI));
+
+  Result.AppendChild(GerarServico);
+
+  nodeArray := GerarServicos;
+  if nodeArray <> nil then
+  begin
+    for i := 0 to Length(nodeArray) - 1 do
+    begin
+      Result.AppendChild(nodeArray[i]);
+    end;
+  end;
+
+  Result.AppendChild(GerarPrestador);
+  Result.AppendChild(GerarTomador);
+  Result.AppendChild(GerarIntermediarioServico);
+  Result.AppendChild(GerarConstrucaoCivil);
+
+  if GerarAtividadeEventoAposConstrucaoCivil then
+    Result.AppendChild(GeraAtividadeEvento);
+
+  Result.AppendChild(AddNode(tcStr, '#6', 'RegimeEspecialTributacao', 1, 2, 0,
+   FpAOwner.RegimeEspecialTributacaoToStr(NFSe.RegimeEspecialTributacao), DSC_REGISSQN));
+
+  Result.AppendChild(AddNode(tcStr, '#7', 'OptanteSimplesNacional', 1, 1, 1,
+               FpAOwner.SimNaoToStr(NFSe.OptanteSimplesNacional), DSC_INDOPSN));
+
+  Result.AppendChild(AddNode(tcStr, '#8', 'IncentivoFiscal', 1, 1, 0,
+              FpAOwner.SimNaoToStr(NFSe.IncentivadorCultural), DSC_INDINCCULT));
+
+  Result.AppendChild(AddNode(tcStr, '#1', 'RegimeApuracaoSN', 1, 1, 1,
+                             RegimeApuracaoSNToStr(NFSe.RegimeApuracaoSN), ''));
+
+  Result.AppendChild(GerarIBSCBS(NFSe.IBSCBS));
+end;
+
+function TNFSeW_Saatri203.GerarServico: TACBrXmlNode;
+begin
+  Result := inherited GerarServico;
+
+  Result.AppendChild(AddNode(tcStr, '#27', 'CSTPisCofins', 1, 15, 0,
+                               CSTToStr(NFSe.Servico.Valores.tribFed.CST), ''));
+
+  Result.AppendChild(AddNode(tcStr, '#28', 'RetencaoPisCofinsCsll', 1, 15, 0,
+         tpRetPisCofinsToStr(NFSe.Servico.Valores.tribFed.tpRetPisCofins), ''));
+end;
+
+function TNFSeW_Saatri203.GerarValores: TACBrXmlNode;
+begin
+  Result := inherited GerarValores;
+
+  Result.AppendChild(AddNode(tcDe2, '#27', 'AliquotaPis', 1, 15, 0,
+                                         NFSe.Servico.Valores.AliquotaPis, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#28', 'AliquotaCofins', 1, 15, 0,
+                                      NFSe.Servico.Valores.AliquotaCofins, ''));
+end;
+
+function TNFSeW_Saatri203.GerarIBSCBS(IBSCBS: TIBSCBSDPS): TACBrXmlNode;
+begin
+  Result := CreateElement('IbsCbs');
+
+  Result.AppendChild(AddNode(tcInt, '#1', 'MunicipioIncidencia', 7, 7, 1,
+                                     NFSe.infNFSe.IBSCBS.cLocalidadeIncid, ''));
+
+  Result.AppendChild(AddNode(tcStr, '#1', 'IndicadorFinalidade', 1, 1, 1,
+                                           indFinalToStr(IBSCBS.indFinal), ''));
+
+  Result.AppendChild(AddNode(tcStr, '#1', 'IndicadorOperacao', 6, 6, 1,
+                                                            IBSCBS.cIndOp, ''));
+
+  Result.AppendChild(AddNode(tcStr, '#1', 'ClassificacaoTributaria', 6, 6, 1,
+                                   IBSCBS.valores.trib.gIBSCBS.cClassTrib, ''));
+
+  Result.AppendChild(AddNode(tcStr, '#1', 'IndicadorDestino', 1, 1, 1,
+                                             indDestToStr(IBSCBS.indDest), ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'BaseCalculo', 1, 15, 1,
+                                          NFSe.infNFSe.IBSCBS.valores.vBC, ''));
+
+//  Result.AppendChild(AddNode(tcDe2, '#1', 'ValorCalcReeRepRes', 1, 15, 0,
+//                NFSe.IBSCBS.valores.gReeRepRes.documentos[0].vlrReeRepRes, ''));
+
+  Result.AppendChild(GerarIBSMunicipal);
+  Result.AppendChild(GerarIBSEstadual);
+  Result.AppendChild(GerarCBS);
+end;
+
+function TNFSeW_Saatri203.GerarIBSMunicipal: TACBrXmlNode;
+begin
+  Result := CreateElement('IbsMunicipal');
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Percentual', 1, 7, 1,
+                                  NFSe.infNFSe.IBSCBS.valores.Mun.pIBSMun, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercRedAliq', 1, 7, 0,
+                              NFSe.infNFSe.IBSCBS.valores.Mun.pRedAliqMun, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercAliqEfet', 1, 7, 1,
+                             NFSe.infNFSe.IBSCBS.valores.Mun.pAliqEfetMun, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Valor', 1, 15, 1,
+                      NFSe.infNFSe.IBSCBS.totCIBS.gIBS.gIBSMunTot.vIBSMun, ''));
+end;
+
+function TNFSeW_Saatri203.GerarIBSEstadual: TACBrXmlNode;
+begin
+  Result := CreateElement('IbsEstadual');
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Percentual', 1, 7, 1,
+                              NFSe.infNFSe.IBSCBS.valores.UF.pIBSUF, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercRedAliq', 1, 7, 0,
+                                NFSe.infNFSe.IBSCBS.valores.UF.pRedAliqUF, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercAliqEfet', 1, 7, 1,
+                               NFSe.infNFSe.IBSCBS.valores.UF.pAliqEfetUF, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Valor', 1, 15, 1,
+                        NFSe.infNFSe.IBSCBS.totCIBS.gIBS.gIBSUFTot.vIBSUF, ''));
+end;
+
+function TNFSeW_Saatri203.GerarCBS: TACBrXmlNode;
+begin
+  Result := CreateElement('Cbs');
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Percentual', 1, 7, 1,
+                                     NFSe.infNFSe.IBSCBS.valores.fed.pCBS, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercRedAliq', 1, 7, 0,
+                              NFSe.infNFSe.IBSCBS.valores.fed.pRedAliqCBS, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'PercAliqEfet', 1, 7, 1,
+                             NFSe.infNFSe.IBSCBS.valores.fed.pAliqEfetCBS, ''));
+
+  Result.AppendChild(AddNode(tcDe2, '#1', 'Valor', 1, 15, 1,
+                                    NFSe.infNFSe.IBSCBS.totCIBS.gCBS.vCBS, ''));
 end;
 
 end.
