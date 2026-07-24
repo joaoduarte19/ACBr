d0b956b3f9f56db5dc151bcaf2ae2c6404fb51d3
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-07-24 13:59:54 +0000

    24/07/2026 -- ACBrNFSeXConversao -- [-] ACBR-9593 - Incluído o enumerario trVazio ao tipo TtpRetPisCofins uma vez que a     a tag é opcional.    Por: Valter Patrick/Italo Giurizzato Junior
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@47364 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Base/ACBrNFSeXConversao.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Base/ACBrNFSeXConversao.pas
@@ -557,11 +557,12 @@ type
                      trpcNaoRetido, trpiscofinscsllRetido,
                      trpiscofinsRetidocsllNaoRetido, trPisRetidoCofinsCsllNaoRetido,
                      trCofinsRetidoPisCsllNaoRetido, trCofinsCsllRetidoPisNaoRetido,
-                     trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido);
+                     trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido,
+                     trVazio);
 
 const
   TtpRetPisCofinsArrayStrings: array[TtpRetPisCofins] of string = ('0', '1', '2',
-    '3', '4', '5', '6', '7', '8', '9');
+    '3', '4', '5', '6', '7', '8', '9', '');
 
 type
   TindTotTrib = (indNao, indSim);
@@ -13707,23 +13708,25 @@ end;
 function tpRetPisCofinsToStr(const t: TtpRetPisCofins): string;
 begin
   result := EnumeradoToStr(t,
-                           ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                           ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ''],
               [trpiscofinscsllNaoRetido, trpcRetido,
                trpcNaoRetido, trpiscofinscsllRetido,
                trpiscofinsRetidocsllNaoRetido, trPisRetidoCofinsCsllNaoRetido,
                trCofinsRetidoPisCsllNaoRetido, trCofinsCsllRetidoPisNaoRetido,
-               trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido]);
+               trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido,
+               trVazio]);
 end;
 
 function StrTotpRetPisCofins(out ok: Boolean; const s: string): TtpRetPisCofins;
 begin
   result := StrToEnumerado(ok, s,
-                           ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                           ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ''],
               [trpiscofinscsllNaoRetido, trpcRetido,
                trpcNaoRetido, trpiscofinscsllRetido,
                trpiscofinsRetidocsllNaoRetido, trPisRetidoCofinsCsllNaoRetido,
                trCofinsRetidoPisCsllNaoRetido, trCofinsCsllRetidoPisNaoRetido,
-               trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido]);
+               trCsllRetidoPisCofinsNaoRetido, trPisCsllRetidoCofinsNaoRetido,
+               trVazio]);
 end;
 
 function tpRetPisCofinsDescricao(const t: TtpRetPisCofins): string;
