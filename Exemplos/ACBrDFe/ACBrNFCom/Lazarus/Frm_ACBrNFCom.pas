22cd47ca7410384d432e2b73ac67025b0b47c72c
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2026-09-01 20:05:32 +0000

    [-] Atualiza nomenclatura do pIBS e vIBS para coincidir com atualização. Por: Diego Folieni Veja: https://www.projetoacbr.com.br/forum/topic/94539-fim-do-uso-da-unit-pcnconversao-nos-dfe-e-uso-das-units-exclusivas-para-a-rtc/
    git-svn-id: https://svn.code.sf.net/p/acbr/code/trunk2@48061 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrDFe/ACBrNFCom/Lazarus/Frm_ACBrNFCom.pas
+++ Exemplos/ACBrDFe/ACBrNFCom/Lazarus/Frm_ACBrNFCom.pas
@@ -529,21 +529,21 @@ begin
 
           IBSCBS.gIBSCBS.vBC := 100;
 
-          IBSCBS.gIBSCBS.gIBSUF.pIBS := 5;
+          IBSCBS.gIBSCBS.gIBSUF.pIBSUF := 5;
           IBSCBS.gIBSCBS.gIBSUF.gDif.pDif := 5;
           IBSCBS.gIBSCBS.gIBSUF.gDif.vDif := 50;
           IBSCBS.gIBSCBS.gIBSUF.gDevTrib.vDevTrib := 50;
           IBSCBS.gIBSCBS.gIBSUF.gRed.pRedAliq := 5;
           IBSCBS.gIBSCBS.gIBSUF.gRed.pAliqEfet := 5;
-          IBSCBS.gIBSCBS.gIBSUF.vIBS := 50;
+          IBSCBS.gIBSCBS.gIBSUF.vIBSUF := 50;
 
-          IBSCBS.gIBSCBS.gIBSMun.pIBS := 5;
+          IBSCBS.gIBSCBS.gIBSMun.pIBSMun := 5;
           IBSCBS.gIBSCBS.gIBSMun.gDif.pDif := 5;
           IBSCBS.gIBSCBS.gIBSMun.gDif.vDif := 50;
           IBSCBS.gIBSCBS.gIBSMun.gDevTrib.vDevTrib := 50;
           IBSCBS.gIBSCBS.gIBSMun.gRed.pRedAliq := 5;
           IBSCBS.gIBSCBS.gIBSMun.gRed.pAliqEfet := 5;
-          IBSCBS.gIBSCBS.gIBSMun.vIBS := 50;
+          IBSCBS.gIBSCBS.gIBSMun.vIBSMun := 50;
 
           // vIBS = vIBS do IBSUF + vIBS do IBSMun
           IBSCBS.gIBSCBS.vIBS := 100;
@@ -1743,7 +1743,7 @@ begin
 
   if ACBrNFCom1.DANFCom <> nil then
   begin
-    ACBrNFCom1.DANFCom.TipoDANFCom := StrToTpImp(OK, IntToStr(rgTipoDaNFCom.ItemIndex + 1));
+    ACBrNFCom1.DANFCom.TipoDANFCom := StrToTpImp(IntToStr(rgTipoDaNFCom.ItemIndex + 1));
     ACBrNFCom1.DANFCom.Logo := edtLogoMarca.Text;
   end;
 end;
